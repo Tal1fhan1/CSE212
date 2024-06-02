e2756c52f5e93f211eca3330fa910d97f484138e
Tal1fhan1 <talifhaninetshiheni@gmail.com>
2024-06-02 07:31:25 +0200

    W06 Code: Trees

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,35 +1,86 @@
-public class Node {
+public class Node
+{
     public int Data { get; set; }
     public Node? Right { get; private set; }
     public Node? Left { get; private set; }
 
-    public Node(int data) {
+    public Node(int data)
+    {
         this.Data = data;
     }
 
-    public void Insert(int value) {
-        if (value < Data) {
+    public void Insert(int value)
+    {
+        if (value < Data)
+        {
             // Insert to the left
             if (Left is null)
                 Left = new Node(value);
-            else
+            else if (Left.Contains(value) == false)
                 Left.Insert(value);
         }
-        else {
+        else
+        {
             // Insert to the right
             if (Right is null)
                 Right = new Node(value);
-            else
+            else if (Right.Contains(value) == false)
                 Right.Insert(value);
         }
     }
 
-    public bool Contains(int value) {
+    public bool Contains(int value)
+    {
+        if (value == Data)
+        {
+            return true;
+        }
+
+        if (value < Data)
+        {
+            if (Left is null)
+            {
+                return false;
+            }
+
+            else if (Left is not null)
+            {
+                if (Left.Data == value)
+                {
+                    return true;
+                }
+                else
+                {
+                    Left.Contains(value);
+                }
+            }
+        }
+
+        else if (value > Data)
+        {
+            if (Right is null)
+            {
+                return false;
+            }
+
+            else if (Right is not null)
+            {
+                if (Right.Data == value)
+                {
+                    return true;
+                }
+                else
+                {
+                    Right.Contains(value);
+                }
+            }
+        }
         // TODO Start Problem 2
         return false;
     }
 
-    public int GetHeight() {
+    public int GetHeight()
+    {
         // TODO Start Problem 4
         return 0; // Replace this line with the correct return statement(s)
     }
